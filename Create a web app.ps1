996311768b51828d9096fb0339dfd2155d1c0678
David Bourke <davidrbourke@gmail.com>
2018-06-07 20:58:54 +0100

    Added creating web apps

---
+++ Create a web app.ps1
@@ -0,0 +1,23 @@
+﻿$resourceGroupName = 'davidbourkerg02'
+$appServicePlanName = 'davidbourkeap02'
+$webappName = 'davidbourkewebapp02'
+$location = 'UK South'
+
+#Login-AzureRmAccount
+<#
+$subscription = Get-AzureRmSubscription
+
+Set-AzureRmContext -SubscriptionId $subscription[0].Id
+
+New-AzureRmResourceGroup -Name $resourceGroupName -Location $location
+
+New-AzureRmAppServicePlan -Name $appServicePlanName -Location $location -ResourceGroupName $resourceGroupName `
+-Tier Free
+
+New-AzureRmWebApp -ResourceGroupName $resourceGroupName -AppServicePlan $appServicePlanName -Location $location `
+-Name $webappName
+#>
+
+$webAppObj = Get-AzureRmWebApp -ResourceGroupName $resourceGroupName -Name $webappName
+
+$appSettings = $webAppObj.SiteConfig.AppSettings
